d8b7b1951ee2f48e2e7581bed313d70c0c755568
Kay Bothfeld <bothfeld@scio.de>
2014-05-15 17:50:36 +0200

    Interim before integrating subtree https://github.com/kayy/SmartFormat

---
+++ Editor/TTT.cs
@@ -0,0 +1,31 @@
+// Created by Kay
+// Copyright 2013 by SCIO System-Consulting GmbH & Co. KG. All rights reserved.
+using System.IO;
+using System.Collections.Generic;
+
+public class TTT
+{
+	public string Name = "Kay";
+	public int age = 49;
+	public List<TTT> Friends = new List<TTT> ();
+	public TTT (string name)
+	{
+		Name = name;
+	}
+	public void TestCaseUser ()
+	{
+//		TTT me = new TTT ("Kay");
+//		TTT annette = new TTT ("Annette");
+//		annette.Friends.Add (new TTT ("Ariane"));
+//		me.Friends.Add (annette);
+//		TTT undine = new TTT ("Undine");
+//		me.Friends.Add (undine);
+//		SmartFormat.Smart.Default.ErrorAction = SmartFormat.Core.ErrorAction.OutputErrorInResult;
+//		SmartFormat.Smart.Default.Parser.ErrorAction = SmartFormat.Core.ErrorAction.ThrowError;
+//		
+//		SmartFormat.Smart.Default.Parser.UseBraceEscaping ();
+//		StreamReader reader = new StreamReader ("/Users/kay/Development/git/SpiceInvaders/SpiceInvaders/Assets/Editor/AnimatorWrapper/Editor/AnimatorWrapper.txt"); 
+//		string output = SmartFormat.Smart.Format(reader.ReadToEnd (), this);
+//		UnityEngine.Debug.Log (output);
+	}
+}
